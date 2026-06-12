27037b0da07cbbad899502da95168ce9ec5f0eeb
google644 <google@sensabien.com>
2026-06-12 11:08:06 +0200

    fix: rename class to Zeyvro_Turnstile (PS8 install bug) + bump v1.0.5
    PS8 coreLoadModule hace class_exists() con el nombre literal del modulo;
    con name zeyvro_turnstile la clase debe llevar el underscore.
    RUNTIME 4/4 PASS (install+uninstall PHP 8.0-8.3, PS 8.2.6).

--- controllers/admin/AdminZeyvroTurnstileController.php
+++ controllers/admin/AdminZeyvroTurnstileController.php
@@ -208,7 +208,7 @@ class AdminZeyvroTurnstileController extends ModuleAdminController
         ];
 
         $fieldsValue = [];
-        foreach (ZeyvroTurnstile::CONFIG_KEYS as $k) {
+        foreach (Zeyvro_Turnstile::CONFIG_KEYS as $k) {
             $fieldsValue[$k] = Configuration::get($k);
         }
 
